173536be39fb7ac3455b9456ecf856940ea106b0
Phil Kunz <gitlab@philkunz.com>
2019-05-05 17:41:39 +0200

    fix(core): update

--- test/test.ts
+++ test/test.ts
@@ -3,7 +3,7 @@ import * as smartmarkdown from '../ts/index';
 
 tap.test('first test', async () => {
   const mdString = smartmarkdown.markdownToHtml('# Hi!');
-  expect(mdString).to.equal('<h1 id="hi-">Hi!</h1>\n');
+  expect(mdString).to.equal('<h1 id="hi">Hi!</h1>\n');
 });
 
 tap.start();
